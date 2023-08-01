e36cc6f0d939bcd0fffa979ace41f56c7dc2f945
Petr Jindra <petr@jindrovi.uno>
2023-08-01 20:10:35 +0200

    creating ServerConnection with api and stream api threads

--- src/connection/connection.rs
+++ src/connection/connection.rs
@@ -8,38 +8,112 @@ use tungstenite::Message;
 use crate::connection::socket::{Socket, SocketError};
 use crate::connection::SocketConfig;
 
-pub struct ServerConnection {}
+pub struct ServerConnection {
+    api_service: SocketServiceHandle,
+    stream_api_service: SocketServiceHandle
+}
 
 
 impl ServerConnection {
     pub fn new(socket_config: SocketConfig, stream_socket_config: SocketConfig) -> Result<Self, ServerConnectionError> {
+        let api_service = Self::make_socket_service(socket_config)?;
+        let stream_api_service = Self::make_socket_service(stream_socket_config)?;
+        Ok(Self {
+            api_service,
+            stream_api_service,
+        })
+    }
+
+    fn make_socket_service(socket_config: SocketConfig) -> Result<SocketServiceHandle, ServerConnectionError> {
         let socket = Socket::new(socket_config)?;
-        let stream_socket = Socket::new(stream_socket_config)?;
-        todo!()
+        Ok(SocketServiceHandle::new(socket))
     }
 }
 
 
-fn wrap_socket(socket: Socket) -> (Sender<SocketControlCommand>, Receiver<SocketNotification>, JoinHandle<()>) {
-    let mut socket = socket;
-    let (input_sender, input_receiver) = channel();
-    let (output_sender, output_receiver) = channel();
-    let handle = thread::spawn(move || {
+struct SocketServiceHandle {
+    input: Sender<SocketControlCommand>,
+    output: Receiver<SocketNotification>,
+    join: JoinHandle<()>
+}
+
+
+impl SocketServiceHandle {
+    fn new(socket: Socket) -> Self {
+        let (mut service, input, output) = SocketService::new(socket);
+        let join = thread::spawn(move || {
+            service.start();
+        });
+        Self {
+            input,
+            output,
+            join,
+        }
+    }
+}
+
+
+struct SocketService {
+    socket: Socket,
+    command_input: Receiver<SocketControlCommand>,
+    notification_output: Sender<SocketNotification>,
+    stop_flag: bool,
+}
+
+
+impl SocketService {
+    fn new(socket: Socket) -> (Self, Sender<SocketControlCommand>, Receiver<SocketNotification>) {
+        let (cmd_sender, cmd_receiver) = channel();
+        let (notification_sender, notification_receiver) = channel();
+        let instance = Self {
+            socket,
+            command_input: cmd_receiver,
+            notification_output: notification_sender,
+            stop_flag: false,
+        };
+        (instance, cmd_sender, notification_receiver)
+    }
+
+    fn start(&mut self) {
         loop {
-            if let Ok(cmd) = input_receiver.try_recv() {
-                match cmd {
-                    SocketControlCommand::Stop => break,
-                    SocketControlCommand::Send(msg) => socket.send(msg)
-                        .or_else(|err| {
-                            output_sender.send(SocketNotification::SocketError(err))
-                        }).unwrap()
-                }
+            self.receive_messages();
+            self.process_commands();
+            if self.stop_flag {
+                break;
             }
         }
-        output_sender.send(SocketNotification::Stopped).unwrap();
-    });
-    (input_sender, output_receiver, handle)
+        self.notification_output.send(SocketNotification::Stopped).unwrap();
+    }
+
+    fn process_commands(&mut self) {
+        while let Ok(cmd) = self.command_input.try_recv() {
+            match cmd {
+                SocketControlCommand::Stop => self.stop(),
+                SocketControlCommand::Send(msg) => self.send_message(msg),
+            }
+        }
+    }
+
+    fn stop(&mut self) {
+        self.stop_flag = true;
+    }
+
+    fn send_message(&mut self, message: Message) {
+        match self.socket.send(message) {
+            Err(err) => self.send_notification(SocketNotification::SocketError(err)),
+            _ => ()
+        }
+    }
+
+    fn receive_messages(&mut self) {
+
+    }
+
+    fn send_notification(&mut self, notification: SocketNotification) {
+        self.notification_output.send(notification).unwrap();
+    }
 }
+
 
 enum SocketControlCommand {
     Stop,
@@ -48,6 +122,7 @@ enum SocketControlCommand {
 
 enum SocketNotification {
     Stopped,
+    Message(Message),
     SocketError(SocketError),
 }
 
