25055094344ed6ddc5ed6040cfb4bf21a79f3e68
Petr Jindra <petr@jindrovi.uno>
2023-08-01 14:24:55 +0200

    better socket configuration + enabling TLS feature for tungstenite crate

--- src/connection/socket.rs
+++ src/connection/socket.rs
@@ -1,7 +1,8 @@
 use std::net::TcpStream;
+use std::time::Duration;
 use thiserror::Error;
 use tungstenite::stream::MaybeTlsStream;
-use tungstenite::{connect, WebSocket, Error as TungsteniteError};
+use tungstenite::{connect, WebSocket, Error as TungsteniteError, Message};
 use url::{Url, ParseError};
 
 /// Hold configuration for Socket
@@ -12,6 +13,8 @@ pub struct SocketConfig {
     pub host: String,
     /// Endpoint on the host where to connect
     pub endpoint: String,
+    /// Socket read timeout in ms
+    pub read_timeout: Option<u64>,
 }
 
 
@@ -23,16 +26,39 @@ pub struct Socket {
 
 impl Socket {
     pub fn new(config: SocketConfig) -> Result<Self, SocketError> {
-        let url = Self::build_url(config)?;
-        let (socket, _) = connect(url).map_err(|err| SocketError::ConnectionError(err))?;
+        let socket = Self::make_socket(config)?;
         Ok(Self {
             socket
         })
     }
 
-    fn build_url(config: SocketConfig) -> Result<Url, SocketError> {
+    pub fn send(&mut self, message: Message) -> Result<(), SocketError> {
+        self.socket.send(message).map_err(|err| SocketError::UnableToSendMessage(err))
+    }
+
+    fn make_socket(config: SocketConfig) -> Result<WebSocket<MaybeTlsStream<TcpStream>>, SocketError> {
+        let url = Self::build_url(&config)?;
+        let (mut socket, _) = connect(url).map_err(|err| SocketError::UnableToConnect(err))?;
+        if let Some(timeout) = config.read_timeout {
+            Self::set_timeout(&mut socket, timeout)?;
+        }
+        Ok(socket)
+    }
+
+    fn build_url(config: &SocketConfig) -> Result<Url, SocketError> {
         let url_base = format!("{}://{}/{}", config.protocol, config.host, config.endpoint);
         Ok(Url::parse(&url_base)?)
+    }
+
+    fn set_timeout(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>, timeout: u64) -> Result<(), SocketError> {
+        let mut socket = socket;
+        let dur = Some(Duration::from_millis(timeout));
+        match socket.get_mut() {
+            MaybeTlsStream::Plain(s) => s.set_read_timeout(dur),
+            MaybeTlsStream::NativeTls(s) => s.get_mut().set_read_timeout(dur),
+            _ => Ok(())
+        }.map_err(|_| SocketError::UnableToSetTimeout)?;
+        Ok(())
     }
 }
 
@@ -41,7 +67,11 @@ pub enum SocketError {
     #[error("URL is malformed")]
     InvalidUrl(ParseError),
     #[error("Unable to connect")]
-    ConnectionError(TungsteniteError)
+    UnableToConnect(TungsteniteError),
+    #[error("Unable to connect")]
+    UnableToSendMessage(TungsteniteError),
+    #[error("Unable to set timeout")]
+    UnableToSetTimeout,
 }
 
 
