f1664e14c216ebe14703d0b8c32dff0ae176eb6c
Petr Jindra <petr@jindrovi.uno>
2023-08-03 11:22:57 +0200

    public exports of command module members

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,2 +1,5 @@
+pub use command::{Command, CommandError, CommandOk, CommandResult};
+pub use connection::{Response, ResponseError, XtbServerConnection, XtbServerConnectionError};
+
 mod command;
 mod connection;
